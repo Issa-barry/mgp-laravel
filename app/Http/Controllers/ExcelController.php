5e0ed66a6d278cf61547818b9b6ab5989a74327f
Issa-barry <issabarry67@gmail.com>
2025-04-18 18:05:22 +0200

    rr

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -95,7 +95,7 @@ class ExcelController extends Controller
             //     $rowIndex++;
             // }
 
-            // Si la ligne est dans les lignes mises en surbrillance, avec coloriage en jaune
+            // Si la ligne est dans les lignes mises en surbrillance, avec coloriage en jaune.
             if (in_array($i, $highlightedRows)) {
                 $matchedSheet->fromArray(array_values($row), null, "A$rowIndex");
                 $matchedSheet->getStyle("A$rowIndex:Z$rowIndex")->getFill()->setFillType('solid')
@@ -103,7 +103,7 @@ class ExcelController extends Controller
                 $rowIndex++;
             }
             
-        }
+        } 
         (new Xlsx($matchedSpreadsheet))->save(storage_path('app/public/matched.xlsx'));
 
         // Statistiques
